7f28fa630da24c4aca2a598d6d685cd17601ac6e
mattPelleg <pmatteo0@gmail.com>
2023-07-06 10:43:35 +0200

    prova commit fede

--- CatalogoProdotti/src/main/java/com/azienda/catalogoProdotti/ui/InitServlet.java
+++ CatalogoProdotti/src/main/java/com/azienda/catalogoProdotti/ui/InitServlet.java
@@ -33,6 +33,10 @@ public class InitServlet extends HttpServlet {
 	public void init() throws ServletException {
 
 		EntityManager em = null;
+		
+		/*********************************
+		  SCORREGGIA
+		 *********************************/
 
 		try {
 
